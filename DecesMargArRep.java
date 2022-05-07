ba8968139fc94a4215a3f45e32e0489fda6bdeec
hajarkahem <94306051+hajarkahem@users.noreply.github.com>
2022-05-07 17:12:49 +0200

    Delete DecesMargArRep.java

--- DecesMargArRep.java
+++
@@ -1,7 +0,0 @@
-package com.civil.project.dao;
-
-import com.civil.project.entity.MarginaleDecesAr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DecesMargArRep extends JpaRepository<MarginaleDecesAr,Integer> {
-}

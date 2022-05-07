8c35ace880a6811504699b5a412b979f3664070d
hajarkahem <94306051+hajarkahem@users.noreply.github.com>
2022-05-07 17:19:38 +0200

    deces service

---
+++ src/main/java/com/civil/project/service/RegistreDecesServiceImlp.java
@@ -0,0 +1,47 @@
+package com.civil.project.service;
+
+import com.civil.project.dao.DecesRegistreRep;
+import com.civil.project.entity.RegistreDeces;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RegistreDecesServiceImlp implements RegistreDecesService {
+
+    @Autowired
+    DecesRegistreRep rep;
+
+    @Override
+    public void ajouterRegistreD(RegistreDeces registreDeces) {
+        rep.save(registreDeces);
+
+    }
+
+    @Override
+    public void modifierRegistreD(RegistreDeces registreDeces) {
+        rep.save(registreDeces);
+
+    }
+
+    @Override
+    public RegistreDeces trouverParId(int i) {
+        Optional<RegistreDeces> result=rep.findById(i);
+        return result.get() ;
+    }
+
+    @Override
+    public List<RegistreDeces> listerRegistreD() {
+        return rep.findAll();
+    }
+
+    @Override
+    public void supprimerRegistreD(int i) {
+        rep.deleteById(i);
+
+    }
+}

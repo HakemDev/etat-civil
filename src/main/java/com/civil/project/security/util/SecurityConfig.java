b7a643b771f69ebe87f8929abaafa71d9e7b363f
omar <omargrinat9@gmail.com>
2022-06-10 19:54:02 +0100

    Add authorization

--- src/main/java/com/civil/project/security/util/SecurityConfig.java
+++ src/main/java/com/civil/project/security/util/SecurityConfig.java
@@ -4,20 +4,29 @@ import com.civil.project.security.filter.JwtRequestFilter;
 import com.civil.project.security.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -25,6 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final MyUserDetailsService myUserDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
+    private final String[] paths = {"/deces/**","/marginales-deces-ar/**"
+    ,"/marginales-deces-fr/**", "/registre-actes-deces/**","/juge-deces/**",
+    "/marginales-juges-deces-ar/**", "/marginales-juges-deces-fr/**","/registre-juges-deces/**",
+    "/juges-naissance/**", "/marginales-juges-naissances-ar/**","/marginales-juges-naissances-fr/**",
+    "/registres-juges-naissance/**","/acte/**","/marginales-fr/**","/marginales-ar/**",
+    "/registre-naissance/**",
+    };
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,8 +52,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
                 .antMatchers("/auth/verify").authenticated()
-                .antMatchers("/**").permitAll();
-                //.anyRequest().authenticated();
+                .antMatchers(HttpMethod.PUT,paths).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,paths).hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().
+                exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler());
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -60,7 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedMethods(Arrays.asList("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(
                 Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
@@ -69,5 +89,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return  source;
 
     }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (req, res, e) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                "Vous n'êtes pas autorisé à effectuer cette opération");
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (req, res, e) -> res.sendError(HttpServletResponse.SC_FORBIDDEN,
+                "Vous n'êtes pas autorisé à effectuer cette opération");
+    }
+
+
 
 }
